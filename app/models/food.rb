8fc9a9d7a3f866fa2c5b93b9b92fd851fe5d34e1
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-24 16:21:10 -0500

    Fix eager_loading Food

--- app/models/food.rb
+++ app/models/food.rb
@@ -7,10 +7,10 @@ class Food < ApplicationRecord
 
   def amount_food(current_user)
     value = 0
-    recipes.each do |recipe|
+    recipes.includes([:recipe_foods]).each do |recipe|
       next unless recipe.user_id == current_user.id
 
-      recipe.recipe_foods.each do |recipe_food|
+      recipe.recipe_foods.includes([:food]).each do |recipe_food|
         value += recipe_food.quantity if recipe_food.food.name == name
       end
     end
