b1c61079401afa4db773f731b43ecb673a755020
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-11-24 18:55:01 -0300

    Install Rspec, Capybara, Faker, DBcleaner and Factorybot

---
+++ test/factories/users.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8) }
+    password_confirmation { password }
+  end
+end
