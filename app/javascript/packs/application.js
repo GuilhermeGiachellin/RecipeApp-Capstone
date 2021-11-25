177bd1eb9df5a881bcb2675b6236f64b2744d7b4
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-25 13:43:48 -0500

    Set up bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require('bootstrap')
+require("@fortawesome/fontawesome-free/js/all");
+require('./scripts')
