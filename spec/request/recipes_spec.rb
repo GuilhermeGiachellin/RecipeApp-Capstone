8de6aef359f17a96c03c8434f284561ab4a9b3ea
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-25 00:31:59 -0500

    Remove unnecessary test

--- spec/request/recipes_spec.rb
+++ spec/request/recipes_spec.rb
@@ -2,18 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
    
-   before(:all) do
-        @user = User.new(name: 'Guy', email: 'e@e.com',
-                         password: 'Lorem11111', password_confirmation: 'Lorem11111' )
+  before(:all) do
+        @user = User.new(name: 'Daddy', email: 'user1@gmail.com',
+                         password: 'password123', password_confirmation: 'password123' )
         @user.save
-        @recipe = Recipe.new(user_id: @user.id, name: 'recipe 1',
-                            description: 'Lorem ipsum',
-                            preparation_time: 1, cooking_time: 1, public: true)
-                            
+        @recipe = Recipe.new(user_id: @user.id, name: 'Recipe 1',
+                            description: 'This is a recipe',
+                            preparation_time: 1.5, cooking_time: 1, public: true)                
         @recipe.save
-        p "CONSOLELOG"
-        p @recipe
-    end 
+  end 
     
   describe 'GET #public_recipes' do 
     before { get '/' }
@@ -37,7 +34,7 @@ RSpec.describe 'Recipes', type: :request do
       expect(response).to render_template('show')
     end
     it 'have the right text' do
-      expect(response.body).to include('recipe 1')
+      expect(response.body).to include('Recipe 1')
     end
   end
   
